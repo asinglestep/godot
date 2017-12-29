53c1e51a1ad58dde3ad34ac93b2459accb041d4b
asinglestep <1536326836@qq.com>
2017-12-29 14:16:03 +0800

    update dot

--- dot.go
+++ dot.go
@@ -4,28 +4,44 @@ import (
 	"bytes"
 	"fmt"
 	"io/ioutil"
+	"sort"
 	"strings"
 	"text/template"
 )
 
+// Graph Graph
 type Graph struct {
-	Nodes []*Node
-	Edges []*Edge
+	Nodes Nodes
+	Edges Edges
 	Attr  GraphAttr
 }
 
+// NewGraph NewGraph
 func NewGraph() *Graph {
 	g := &Graph{}
 	g.Attr = GraphAttr{}
 	return g
 }
 
+// AddNode AddNode
+func (g *Graph) AddNode(node *Node) {
+	g.Nodes.Nodes = append(g.Nodes.Nodes, node)
+}
+
+// AddEdge AddEdge
+func (g *Graph) AddEdge(edge *Edge) {
+	g.Edges.Edges = append(g.Edges.Edges, edge)
+}
+
+// GraphAttr GraphAttr
 type GraphAttr map[string]string
 
+// Add Add
 func (attr GraphAttr) Add(key string, val string) {
 	attr[key] = val
 }
 
+// String String
 func (attr GraphAttr) String() string {
 	list := []string{}
 	for k, v := range attr {
@@ -35,13 +51,42 @@ func (attr GraphAttr) String() string {
 	return strings.Join(list, ";\n")
 }
 
+// Node Node
 type Node struct {
 	Name string
 	Attr NodeAttr
 }
 
+// Nodes Nodes
+type Nodes struct {
+	Nodes []*Node
+}
+
+// Len Len
+func (n *Nodes) Len() int {
+	return len(n.Nodes)
+}
+
+// Swap Swap
+func (n *Nodes) Swap(i, j int) {
+	n.Nodes[i], n.Nodes[j] = n.Nodes[j], n.Nodes[i]
+}
+
+// Less Less
+func (n *Nodes) Less(i, j int) bool {
+	return n.Nodes[i].Name < n.Nodes[j].Name
+}
+
+// Sort Sort
+func (n *Nodes) Sort() []*Node {
+	sort.Sort(n)
+	return n.Nodes
+}
+
+// NodeAttr NodeAttr
 type NodeAttr map[string]string
 
+// String String
 func (attr NodeAttr) String() string {
 	list := []string{}
 	for k, v := range attr {
@@ -51,6 +96,7 @@ func (attr NodeAttr) String() string {
 	return strings.Join(list, ",")
 }
 
+// Edge Edge
 type Edge struct {
 	Src     string
 	SrcPort string
@@ -58,22 +104,50 @@ type Edge struct {
 	DstPort string
 }
 
+// Edges Edges
+type Edges struct {
+	Edges []*Edge
+}
+
+// Len Len
+func (e *Edges) Len() int {
+	return len(e.Edges)
+}
+
+// Swap Swap
+func (e *Edges) Swap(i, j int) {
+	e.Edges[i], e.Edges[j] = e.Edges[j], e.Edges[i]
+}
+
+// Less Less
+func (e *Edges) Less(i, j int) bool {
+	return e.Edges[i].Src < e.Edges[j].Src
+}
+
+// Sort Sort
+func (e *Edges) Sort() []*Edge {
+	sort.Sort(e)
+	return e.Edges
+}
+
+// dotTemplate dotTemplate
 const dotTemplate = `
 digraph g {
 	{{ .Attr.String }}
 
 	node [ height=.1, shape=record, width=.1 ];
 
-	{{ range .Nodes }}
+	{{ range .Nodes.Sort }}
 	{{ printf "\"%s\" [ %s ];" .Name .Attr.String }}
 	{{ end }}
 
-	{{ range .Edges }}
+	{{ range .Edges.Sort }}
 	{{ printf "\"%s\"%s -> \"%s\"%s;" .Src .SrcPort .Dst .DstPort }}
 	{{ end }}
 }
 `
 
+// GenerateDotFile 生成dot文件
 func GenerateDotFile(fileName string, g *Graph) error {
 	var buf bytes.Buffer
 
